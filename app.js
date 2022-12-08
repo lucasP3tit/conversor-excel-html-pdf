4f3aa6014153347ea178cd374de02a85f2e07779
Lucas Petit <lucas.a.petit@gmail.com>
2022-12-08 09:14:38 -0300

    convertendo para html

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
 const Reader = require("./Reader");
 const Processor = require("./Processor");
 const Table = require("./Table");
+const Html= require("./HtmlParser");
 
 let reader = new Reader();
 
-async function getDataFile(){
+async function main(){
     let data = await reader.Read("./users.CSV");
     let processedData = Processor.Process(data);
-    let usuarios = new Table(processedData);
-    console.log(usuarios.ColumnsCount);
+    let users = new Table(processedData);
+    
+    let html = await Html.Parser(users);
+
+    console.log(html);
 
     
 }
 
 
-getDataFile();
+main();
